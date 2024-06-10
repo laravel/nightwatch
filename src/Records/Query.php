7738a749e4e376ac06735f27b3844369f9bb0739
Tim MacDonald <hello@timacdonald.me>
2024-06-11 08:36:08 +1000

    wip

---
+++ src/Records/Query.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Laravel\Nightwatch\Records;
+
+class Query
+{
+    /**
+     * @param  non-empty-string  $timestamp
+     * @param  non-empty-string  $group
+     * @param  non-empty-string  $trace_id
+     * @param  'job'|'request'  $execution_context
+     * @param  non-empty-string  $execution_id
+     * @param  non-empty-string  $sql
+     * @param  'select'|'insert'|'update'|'delete'  $category
+     * @param  non-empty-string  $file
+     * @param  non-negative-int  $line
+     * @param  non-negative-int  $duration
+     * @param  non-empty-string  $connection
+     */
+    public function __construct(
+        public string $timestamp,
+        public string $deploy_id,
+        public string $server,
+        public string $group,
+        public string $trace_id,
+        public string $execution_context,
+        public string $execution_id,
+        public string $user,
+        public string $sql,
+        public string $category,
+        public string $file,
+        public int $line,
+        public int $duration,
+        public string $connection,
+    ) {
+        //
+    }
+}
--- src/Sensors/QueriesSensor.php
+++ src/Sensors/QueriesSensor.php
@@ -6,6 +6,7 @@ use Carbon\CarbonImmutable;
 use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Nightwatch\RecordCollection;
+use Laravel\Nightwatch\Records\Query;
 
 final class QueriesSensor
 {
@@ -22,26 +23,26 @@ final class QueriesSensor
     {
         $now = CarbonImmutable::now();
 
-        $this->records['queries'][] = [
+        $this->records['queries'][] = new Query(
             // TODO Can I do this without Carbon?
             // TODO `time` is a float. Does this correctly adjust?
-            'timestamp' => $now->subMilliseconds($event->time)->format('Y-m-d H:i:s'),
-            'deploy_id' => $this->deployId,
-            'server' => $this->server,
-            'group' => hash('sha256', ''), // TODO
-            'trace_id' => $this->traceId,
-            'execution_context' => 'request', // TODO
-            'execution_id' => '00000000-0000-0000-0000-000000000000', // TODO
-            'user' => Auth::id() ?? '', // TODO allow this to be customised
-            'sql' => $event->sql,
-            'category' => 'select', // TODO
-            'file' => 'app/Models/User.php', // TODO
-            'line' => 5, // TODO
+            timestamp: $now->subMilliseconds($event->time)->format('Y-m-d H:i:s'),
+            deploy_id: $this->deployId,
+            server: $this->server,
+            group: hash('sha256', ''), // TODO
+            trace_id: $this->traceId,
+            execution_context: 'request', // TODO
+            execution_id: '00000000-0000-0000-0000-000000000000', // TODO
+            user: Auth::id() ?? '', // TODO allow this to be customised
+            sql: $event->sql,
+            category: 'select', // TODO
+            file: 'app/Models/User.php', // TODO
+            line: 5, // TODO
             // TODO this is done to support the validation. Do we want to track
             // microseconds instead of milliseconds, though?
-            'duration' => (int) $event->time,
-            'connection' => $event->connectionName,
-        ];
+            duration: (int) $event->time,
+            connection: $event->connectionName,
+        );
 
         $executionParent = $this->records['execution_parent'];
 
