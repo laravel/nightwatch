cc5f2c4a37690cdd47298f62738c0e66d6b02be2
Tim MacDonald <hello@timacdonald.me>
2024-06-10 17:52:51 +1000

    wip

--- tests/Feature/Sensors/ExceptionsSensorTest.php
+++ tests/Feature/Sensors/ExceptionsSensorTest.php
@@ -12,6 +12,8 @@ beforeEach(function () {
     setPeakMemoryInKilobytes(1234);
     setTraceId('00000000-0000-0000-0000-000000000000');
     travelTo(CarbonImmutable::parse('2000-01-01 00:00:00'));
+
+    Config::set('app.debug', false);
 });
 
 final class MyException extends RuntimeException
@@ -24,7 +26,6 @@ final class MyException extends RuntimeException
 
 it('ingests exceptions', function () {
     $ingest = fakeIngest();
-    Config::set('app.debug', false);
     Route::post('/users', function () {
         throw new MyException('Whoops!');
     });
@@ -104,5 +105,84 @@ it('ingests exceptions', function () {
     ]);
 });
 
-it('ingests reported exceptions')->todo();
+it('ingests reported exceptions', function () {
+    $ingest = fakeIngest();
+    Route::post('/users', function () {
+        report(new MyException('Whoops!'));
+    });
+
+    $response = post('/users');
+
+    $response->assertOk();
+    $ingest->assertWrittenTimes(1);
+    $ingest->assertLatestWrite([
+        'requests' => [
+            [
+                'timestamp' => '2000-01-01 00:00:00',
+                'deploy_id' => 'v1.2.3',
+                'server' => 'web-01',
+                'group' => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+                'trace_id' => '00000000-0000-0000-0000-000000000000',
+                'method' => 'POST',
+                'route' => '/users',
+                'path' => '/users',
+                'user' => '',
+                'ip' => '127.0.0.1',
+                'duration' => 0,
+                'status_code' => '200',
+                'request_size_kilobytes' => 0,
+                'response_size_kilobytes' => 0,
+                'peak_memory_usage_kilobytes' => 1234,
+                'queries' => 0,
+                'queries_duration' => 0,
+                'lazy_loads' => 0,
+                'lazy_loads_duration' => 0,
+                'jobs_queued' => 0,
+                'mail_queued' => 0,
+                'mail_sent' => 0,
+                'mail_duration' => 0,
+                'notifications_queued' => 0,
+                'notifications_sent' => 0,
+                'notifications_duration' => 0,
+                'outgoing_requests' => 0,
+                'outgoing_requests_duration' => 0,
+                'files_read' => 0,
+                'files_read_duration' => 0,
+                'files_written' => 0,
+                'files_written_duration' => 0,
+                'cache_hits' => 0,
+                'cache_misses' => 0,
+                'hydrated_models' => 0,
+            ],
+        ],
+        'cache_events' => [],
+        'commands' => [],
+        'exceptions' => [
+            [
+                'timestamp' => '2000-01-01 00:00:00',
+                'deploy_id' => 'v1.2.3',
+                'server' => 'web-01',
+                'group' => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+                'trace_id' => '00000000-0000-0000-0000-000000000000',
+                'execution_context' => 'request',
+                'execution_id' => '00000000-0000-0000-0000-000000000000',
+                'user' => '',
+                'class' => 'MyException',
+                'file' => 'app/Models/User.php',
+                'line' => 5,
+                'message' => 'Whoops!',
+                'code' => 0,
+                'trace' => '',
+            ],
+        ],
+        'job_attempts' => [],
+        'lazy_loads' => [],
+        'logs' => [],
+        'mail' => [],
+        'notifications' => [],
+        'outgoing_requests' => [],
+        'queries' => [],
+        'queued_jobs' => [],
+    ]);
+});
 it('can ingest arbitrary exceptions via an event')->todo();
