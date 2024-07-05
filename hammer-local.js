ad3c9c72f141b9520a2dbbcaf18e6f0a0b9865db
Tim MacDonald <hello@timacdonald.me>
2024-07-05 16:29:58 +1000

    wip

--- hammer-local.js
+++ hammer-local.js
@@ -2,16 +2,12 @@ import http from 'k6/http'
 import { check } from 'k6'
 
 export const options = {
-    vus: 20,
-    duration: '3m15s',
-    cloud: {
-        projectID: 3702864,
-        name: "Hammer"
-    },
+    vus: 1,
+    duration: '30s',
 }
 
 export default function() {
-    check(http.get('http://nightwatch-customer.macdonald.au/hammer'), {
+    check(http.get('http://nightwatch-customer.test/hammer'), {
         'response code was 200': (res) => res.status === 200,
         'response content was "ok"': (res) => res.body.toString() === 'ok',
     })
