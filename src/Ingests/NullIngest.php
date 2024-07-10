9ea26fefc401a385f8af648e11f4b015e4021721
Tim MacDonald <hello@timacdonald.me>
2024-07-10 17:22:56 +1000

    wip

--- src/Ingests/NullIngest.php
+++ src/Ingests/NullIngest.php
@@ -15,7 +15,9 @@ final class NullIngest
         $compressed = gzencode($payload);
 
         echo "Payload size: ".number_format(strlen($payload) / 1000 / 1000, 2)." MB";
+        echo PHP_EOL;
         echo "Compressed size: ".number_format(strlen($compressed) / 1000 / 1000, 2)." MB";
+        echo PHP_EOL;
 
         return new Promise(fn ($resolve) => $resolve(
             new IngestSucceededResult(0)
