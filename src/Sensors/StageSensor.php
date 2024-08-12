415af8800f84d09c7390401e96bf0a314af6dbb3
Tim MacDonald <hello@timacdonald.me>
2024-08-12 14:21:29 +1000

    wip

--- src/Sensors/StageSensor.php
+++ src/Sensors/StageSensor.php
@@ -20,7 +20,6 @@ final class StageSensor
         $nowMicrotime = $this->clock->microtime();
 
         $this->executionState->stageDurations[$this->executionState->stage->value] = (int) round(($nowMicrotime - $this->executionState->currentExecutionStageStartedAtMicrotime) * 1_000_000);
-
         $this->executionState->stage = $executionStage;
         $this->executionState->currentExecutionStageStartedAtMicrotime = $nowMicrotime;
     }
