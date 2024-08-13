3b810447b9629683f67904f2e0f87309b92ac438
Tim MacDonald <hello@timacdonald.me>
2024-08-13 16:59:55 +1000

    wip

--- src/Records/Exception.php
+++ src/Records/Exception.php
@@ -30,7 +30,7 @@ final class Exception
         public int $code,
         public string $trace,
         public bool $handled,
-   ) {
+    ) {
         $this->class = Str::tinyText($this->class);
         $this->file = Str::tinyText($this->file);
         $this->message = Str::text($this->message);
