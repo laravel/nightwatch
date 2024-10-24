6b6f0577c1d7f7830757c9ac17e8a9955c0124cd
Tim MacDonald <hello@timacdonald.me>
2024-10-24 14:10:53 +1100

    wip

---
+++ src/Ingests/LogIngest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Laravel\Nightwatch\Ingests;
+
+use Illuminate\Log\LogManager;
+use Laravel\Nightwatch\Contracts\Ingest;
+
+use function json_decode;
+
+/**
+ * @internal
+ */
+final class LogIngest implements Ingest
+{
+    public function __construct(private LogManager $log)
+    {
+        //
+    }
+
+    public function write(string $payload): void
+    {
+        $this->log->info('Nightwatch ingest.', json_decode($payload));
+    }
+}
+
