b873c8291f739ed18594e9e01a78da1c3bde2a38
Ryuta Hamasaki <avosalmon@gmail.com>
2024-11-18 11:22:51 +0900

    Add test for CacheEventListener

---
+++ tests/Unit/Hooks/CacheEventListenerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Cache\Events\CacheHit;
+use Illuminate\Cache\Events\CacheMissed;
+use Illuminate\Cache\Events\KeyWritten;
+use Illuminate\Cache\Events\RetrievingKey;
+use Illuminate\Cache\Events\WritingKey;
+use Laravel\Nightwatch\Hooks\CacheEventListener;
+use Laravel\Nightwatch\SensorManager;
+
+it('gracefully handles exceptions', function () {
+    $sensor = new class extends SensorManager
+    {
+        public bool $thrown = false;
+
+        public function __construct() {}
+
+        public function cacheEvent(RetrievingKey|CacheHit|CacheMissed|WritingKey|KeyWritten $event): void
+        {
+            $this->thrown = true;
+
+            throw new RuntimeException('Whoops!');
+        }
+    };
+
+    $listener = new CacheEventListener($sensor);
+    $event = new RetrievingKey(storeName: 'default', key: 'popular_destinations');
+
+    $listener($event);
+
+    expect($sensor->thrown)->toBeTrue();
+});
