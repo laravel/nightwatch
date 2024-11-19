42a507e1db53a995519c6b2ae5c7e6536e6a6a96
Tim MacDonald <hello@timacdonald.me>
2024-11-20 09:30:17 +1100

    wip

--- tests/Feature/PerformanceTest.php
+++ tests/Feature/PerformanceTest.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Benchmark;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Http;
 
 it('goes fast', function () {
     $config = app('config');
