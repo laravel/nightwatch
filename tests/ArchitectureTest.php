406e39d93058cb5fd4aa5218b6604b472750a2ce
Ryuta Hamasaki <avosalmon@gmail.com>
2024-11-20 11:12:43 +0900

    Remove pest arch presets

--- tests/ArchitectureTest.php
+++ tests/ArchitectureTest.php
@@ -1,21 +1,8 @@
 <?php
 
-arch()->preset()
-    ->laravel()
-    ->ignoring([
-        'App',
-        'Database',
-    ]);
-
-arch()->preset()
-    ->security()
-    ->ignoring([
-        'App',
-        'Database',
-    ]);
-
 arch()->expect('Laravel\\Nightwatch')
     // We should always reference \Symfony\HttpFoundation\Response as not all
     // responses are converted or wrapped in Laravel's response class, e.g.,
     // downloads via `response()->download()` return a Symfony response class.
-    ->not->toUse(\Illuminate\Http\Response::class);
+    ->not->toUse(\Illuminate\Http\Response::class)
+    ->not->toUse(['dd', 'ddd', 'dump', 'ray']);
