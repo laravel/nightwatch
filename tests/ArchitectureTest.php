9645d12b11af8d3bdf5719caf870d9b84a4adaee
Tim MacDonald <hello@timacdonald.me>
2024-11-22 16:00:15 +1100

    Add architecture test

--- tests/ArchitectureTest.php
+++ tests/ArchitectureTest.php
@@ -1,10 +1,16 @@
 <?php
 
 arch()->expect('Laravel\Nightwatch')
-    // We should always reference \Symfony\HttpFoundation\Response as not all
-    // responses are converted or wrapped in Laravel's response class, e.g.,
-    // downloads via `response()->download()` return a Symfony response class.
-    ->not->toUse(\Illuminate\Http\Response::class)
+    ->not->toUse([
+        // We should always reference \Symfony\HttpFoundation\Response as not all
+        // responses are converted or wrapped in Laravel's response class, e.g.,
+        // downloads via `response()->download()` return a Symfony response class.
+        \Illuminate\Http\Response::class,
+        // We should be catching \Throwable.
+        // We should be throwing `RuntimeException`.
+        // This rule can be removed if it doesn't work out well in practice.
+        \Exception::class,
+    ])
     ->not->toUse(['dd', 'ddd', 'dump', 'env', 'ray']);
 
 arch()->expect('Laravel\Nightwatch')
