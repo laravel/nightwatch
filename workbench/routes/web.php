5f1faa34e48bc7f151622e2f51d7dfc0fcb741d3
Ryuta Hamasaki <avosalmon@gmail.com>
2024-11-22 21:07:00 +0900

    Add cache forget to workbench route

--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\UserController;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
@@ -11,8 +10,9 @@ Route::get('/', function () {
     report('Hello world!');
 
     Cache::get('user:55');
-    Cache::put('user:55', 'Taylor');
+    Cache::put('user:55', 'Taylor', 60);
     Cache::get('user:55');
+    Cache::forget('user:55');
 
     return 'ok';
 });
