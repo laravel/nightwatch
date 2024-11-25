0befcc44fbd301252c0df799d6f6ca8e7df51d67
Tim MacDonald <hello@timacdonald.me>
2024-11-25 16:22:36 +1100

    Add http request to kitchen sink (#21)

--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -3,12 +3,15 @@
 use App\Http\UserController;
 use App\Jobs\MyJob;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     DB::table('users')->get('name');
 
     MyJob::dispatch();
+
+    Http::get('https://laravel.com');
 
     report('Hello world!');
 
