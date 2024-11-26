db17506899cc43d6a1676c1422123d3a4cd2defe
Tim MacDonald <hello@timacdonald.me>
2024-11-26 13:08:27 +1100

    Add mail to kitchen sink

--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -2,14 +2,18 @@
 
 use App\Http\UserController;
 use App\Jobs\MyJob;
+use App\Mail\MyMail;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     DB::table('users')->get('name');
 
     MyJob::dispatch();
+
+    Mail::to('tim@laravel.com')->send(new MyMail);
 
     Http::get('https://laravel.com');
 
