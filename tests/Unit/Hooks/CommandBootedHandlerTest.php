48735c2036e1202e60f37d15661a3c9afece0afb
Tim MacDonald <hello@timacdonald.me>
2024-12-02 15:52:30 +1100

    tests

---
+++ tests/Unit/Hooks/CommandBootedHandlerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Laravel\Nightwatch\ExecutionStage;
+use Laravel\Nightwatch\Hooks\CommandBootedHandler;
+use Laravel\Nightwatch\Hooks\RequestBootedHandler;
+use Laravel\Nightwatch\SensorManager;
+
+it('gracefully handles exceptions', function () {
+    $sensor = new class extends SensorManager
+    {
+        public bool $thrown = false;
+
+        public function __construct() {}
+
+        public function stage(ExecutionStage $executionStage): void
+        {
+            $this->thrown = true;
+
+            throw new RuntimeException('Whoops!');
+        }
+    };
+    $handler = new CommandBootedHandler($sensor);
+
+    $handler(app());
+
+    expect($sensor->thrown)->toBeTrue();
+});
