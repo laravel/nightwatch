230003254966ad67b829d4765f8d386e467daf47
Tim MacDonald <hello@timacdonald.me>
2024-12-04 13:26:28 +1100

    Limit records collected

---
+++ tests/Unit/RecordBufferTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use Laravel\Nightwatch\Buffers\RecordsBuffer;
+use Laravel\Nightwatch\ExecutionStage;
+use Laravel\Nightwatch\Records\Mail;
+
+it('only keeps 100 records in memory', function () {
+    $buffer = new RecordsBuffer();
+
+    for ($i = 0; $i < 1_000; $i++) {
+        $buffer->write(new Mail(
+            timestamp: $i / 1000,
+            deploy: '',
+            server: '',
+            _group: '',
+            trace_id: '',
+            execution_source: '',
+            execution_id: '',
+            execution_stage: ExecutionStage::Action,
+            user: '',
+            mailer: '',
+            class: '',
+                subject: '',
+                to: 0,
+                cc: 0,
+                bcc: 0,
+                attachments: 0,
+                duration: 0,
+                failed: false,
+        ));
+    }
+
+    $output = $buffer->flush();
+    expect($output)->not->toContain('"timestamp":0.499,');
+    expect($output)->toContain('"timestamp":0.5,');
+    expect(preg_match_all('/\"t\"\:\"mail\"/', $output))->toBe(500);
+});
