1d9f6bb0809eb881c1c36d92f6ca97bb120fca2d
Tim MacDonald <hello@timacdonald.me>
2024-12-06 08:41:35 +1100

    Gracefully handle middleware being set as string

---
+++ tests/Unit/Hooks/RouteMatchedListenerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Events\RouteMatched;
+use Illuminate\Routing\Route;
+use Laravel\Nightwatch\Hooks\RouteMatchedListener;
+use Laravel\Nightwatch\Hooks\RouteMiddleware;
+
+it('gracefully handles middleware registered as a string', function () {
+    $request = Request::create('/users');
+    $route = new Route(['GET'], '/users', ['middleware' => 'api']);
+    $event = new RouteMatched($route, $request);
+    $handler = new RouteMatchedListener($event);
+
+    expect($route->action['middleware'])->toBe('api');
+
+    $handler($event);
+
+    expect($route->action['middleware'])->toBe(['api', RouteMiddleware::class]);
+});
