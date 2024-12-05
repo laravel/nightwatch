77edc9f59798358e5fdb454f7aa40d2ad130534f
Ryuta Hamasaki <avosalmon@gmail.com>
2024-12-05 12:59:15 +0900

    Add test for JobExceptionOccurredListener

---
+++ tests/Unit/Hooks/JobExceptionOccurredListenerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Queue\Events\JobExceptionOccurred;
+use Illuminate\Queue\Jobs\FakeJob;
+use Laravel\Nightwatch\Hooks\JobExceptionOccurredListener;
+use Laravel\Nightwatch\SensorManager;
+
+it('gracefully handles exceptions', function () {
+    $sensor = new class extends SensorManager
+    {
+        public bool $thrown = false;
+
+        public function __construct() {}
+
+        public function exception(Throwable $e): void
+        {
+            $this->thrown = true;
+
+            throw new RuntimeException('Whoops!');
+        }
+    };
+
+    $listener = new JobExceptionOccurredListener($sensor);
+    $event = new JobExceptionOccurred(
+        'redis',
+        new FakeJob,
+        new RuntimeException('Whoops!')
+    );
+
+    $listener($event);
+
+    expect($sensor->thrown)->toBeTrue();
+});
