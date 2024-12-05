dd07dbb5cfeb2745c209d18cc46325b35c895ca1
Tim MacDonald <hello@timacdonald.me>
2024-12-06 09:34:53 +1100

    Catch exceptions in remaining hooks

--- src/Hooks/ExceptionHandlerResolvedHandler.php
+++ src/Hooks/ExceptionHandlerResolvedHandler.php
@@ -4,7 +4,9 @@ namespace Laravel\Nightwatch\Hooks;
 
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Exceptions\Handler;
+use Illuminate\Support\Facades\Log;
 use Laravel\Nightwatch\SensorManager;
+use Throwable;
 
 final class ExceptionHandlerResolvedHandler
 {
@@ -15,9 +17,13 @@ final class ExceptionHandlerResolvedHandler
 
     public function __invoke(ExceptionHandler $handler): void
     {
-        if ($handler instanceof Handler) {
-            // TODO ensure this isn't a memory leak in Octane
-            $handler->reportable(new ReportableHandler($this->sensor));
+        try {
+            if ($handler instanceof Handler) {
+                // TODO ensure this isn't a memory leak in Octane
+                $handler->reportable(new ReportableHandler($this->sensor));
+            }
+        } catch (Throwable $e) {
+            Log::critical('[nightwatch] '.$e->getMessage());
         }
     }
 }
