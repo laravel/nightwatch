b90a52278f6b9feff69ea7ed290927553dbdeee4
Tim MacDonald <hello@timacdonald.me>
2024-12-09 14:37:26 +1100

    Allow configurable remote ingest

--- src/Factories/AgentFactory.php
+++ src/Factories/AgentFactory.php
@@ -7,10 +7,7 @@ use Illuminate\Support\Env;
 use Laravel\Nightwatch\Buffers\StreamBuffer;
 use Laravel\Nightwatch\Clock;
 use Laravel\Nightwatch\Console\Agent;
-use Laravel\Nightwatch\Contracts\RemoteIngest;
 use React\EventLoop\StreamSelectLoop;
-use React\Http\Browser;
-use React\Socket\ServerInterface;
 
 final class AgentFactory
 {
@@ -43,22 +40,14 @@ final class AgentFactory
         $loop = new StreamSelectLoop;
 
         // Creating an instance of the `TcpServer` will automatically start the
-        // server.  To ensure do not start the server when the command is
-        // constructed, but only when called, we make sure to resolve the
-        // server in the handle method instead instead when constructing.
-        $app->when([Agent::class, 'handle'])
-            ->needs(ServerInterface::class)
-            ->give((new SocketServerFactory($loop, $this->config))(...));
-
-        // Creating an instance of the `Browser` may fail if the configuration
-        // values are incorrect, for example, an empty string for the ingest
-        // URI.  To ensure we do not throw an exception when the command is
-        // resolved, which happens simply by running the `php artisan list`
-        // command, we we make sure to resolve the server in the handle method
-        // instead of when constructing.
-        $app->when([Agent::class, 'handle'])
-            ->needs(RemoteIngest::class)
-            ->give((new HttpIngestFactory($loop, $this->clock, $this->config, $debug))(...));
+        // server. To ensure we do not start the server when the command is
+        // constructed, which will happen when running the `php artisan list`
+        // command, we make sure to resolve the server only when actually
+        // running the command.
+        $app->bindMethod([Agent::class, 'handle'], fn (Agent $agent, Application $app) => $agent->handle(
+            (new SocketServerFactory($loop, $this->config))($app),
+            (new RemoteIngestFactory($loop, $this->clock, $this->config, $debug))($app),
+        ));
 
         return new Agent(new StreamBuffer, $loop, $this->config['ingests']['socket']['timeout'] ?? 0.5, $debug ? 1 : 10);
     }
