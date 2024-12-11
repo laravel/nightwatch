b042695a3b649bad2088b895bfb0a7f49527d7d8
Tim MacDonald <hello@timacdonald.me>
2024-12-11 16:17:57 +1100

    Add missing duration

--- tests/Feature/Sensors/QueuedJobSensorTest.php
+++ tests/Feature/Sensors/QueuedJobSensorTest.php
@@ -80,6 +80,7 @@ it('can ingest queued jobs', function () {
             'name' => 'MyJob',
             'connection' => 'database',
             'queue' => 'default',
+            'duration' => 0,
         ],
     ]);
 });
@@ -176,6 +177,7 @@ it('captures queued mail', function () {
         'name' => 'MyQueuedMail',
         'connection' => 'database',
         'queue' => 'default',
+        'duration' => 0,
     ]);
 });
 
