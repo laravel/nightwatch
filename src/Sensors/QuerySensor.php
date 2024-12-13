4a270d6359f0748f9cd85efd3b6722c038f65cee
Jess Archer <jess@jessarcher.com>
2024-12-13 15:51:03 +1000

    Capture consistent hashes for inserts regardless of row count and restore real SQL

--- src/Sensors/QuerySensor.php
+++ src/Sensors/QuerySensor.php
@@ -10,8 +10,10 @@ use Laravel\Nightwatch\Records\Query;
 use Laravel\Nightwatch\UserProvider;
 
 use function hash;
+use function in_array;
 use function preg_replace;
 use function round;
+use function str_contains;
 
 /**
  * @internal
@@ -37,30 +39,36 @@ final class QuerySensor
 
         $this->executionState->queries++;
 
-        $sql = match ($event->connection->getDriverName()) {
-            'mariadb',
-            'mysql',
-            'pgsql',
-            'sqlite',
-            'sqlsrv' => preg_replace('/in \([\d?\s,]+\)/', 'in (...?)', $event->sql) ?? $event->sql,
-            default => $event->sql,
-        };
-
         $this->executionState->records->write(new Query(
             timestamp: $this->clock->microtime() - ($event->time / 1000),
             deploy: $this->executionState->deploy,
             server: $this->executionState->server,
-            _group: hash('md5', "{$event->connectionName},{$sql}"),
+            _group: $this->hash($event),
             trace_id: $this->executionState->trace,
             execution_source: $this->executionState->source,
             execution_id: $this->executionState->id,
             execution_stage: $this->executionState->stage,
             user: $this->user->id(),
-            sql: $sql,
+            sql: $event->sql,
             file: $file ?? '',
             line: $line ?? 0,
             duration: $durationInMicroseconds,
             connection: $event->connectionName,
         ));
+    }
+
+    protected function hash(QueryExecuted $event): string
+    {
+        if (! in_array($event->connection->getDriverName(), ['mariadb', 'mysql', 'pgsql', 'sqlite', 'sqlsrv'], true)) {
+            return hash('md5', "{$event->connectionName},{$event->sql}");
+        }
+
+        $sql = preg_replace('/in \([\d?\s,]+\)/', 'in (...?)', $event->sql) ?? $event->sql;
+
+        if (str_contains($sql, 'insert')) {
+            $sql = preg_replace('/values [(?,\s\n)]+/i', 'values ...', $sql) ?? $sql;
+        }
+
+        return hash('md5', "{$event->connectionName},{$sql}");
     }
 }
