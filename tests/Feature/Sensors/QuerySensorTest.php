43cfd0b0f6c80e9fa3c56337dca2591f9c75af35
Jess Archer <jess@jessarcher.com>
2024-12-13 15:51:03 +1000

    Capture consistent hashes for inserts regardless of row count and restore real SQL

--- tests/Feature/Sensors/QuerySensorTest.php
+++ tests/Feature/Sensors/QuerySensorTest.php
@@ -142,7 +142,7 @@ it('always uses current time minus execution time for the timestamp', function (
     $ingest->assertLatestWrite('query:0.timestamp', 946688523.466665);
 });
 
-it('collapses variadic "where in" binding placeholders and raw integer values', function ($sql, $expected, $connection) {
+test('group hash collapses variadic "where in" binding placeholders and raw integer values', function ($sql, $expected, $connection) {
     $ingest = fakeIngest();
     Route::get('/users', function () use ($sql, $connection) {
         Event::dispatch(new QueryExecuted($sql, [], 1, $connection));
@@ -152,36 +152,92 @@ it('collapses variadic "where in" binding placeholders and raw integer values', 
 
     $response->assertOk();
     $ingest->assertWrittenTimes(1);
-    $ingest->assertLatestWrite('query:0.sql', $expected);
+    $ingest->assertLatestWrite('query:0.sql', $sql);
+    $ingest->assertLatestWrite('query:0._group', $expected);
 })->with([
     'mysql' => [
         'select * from `users` where `users`.`id` in (1, 2, 3) and `id` in (?, ?, ?)',
-        'select * from `users` where `users`.`id` in (...?) and `id` in (...?)',
+        hash('md5', 'foo,select * from `users` where `users`.`id` in (...?) and `id` in (...?)'),
         new MySqlConnection('test', config: ['name' => 'foo', 'driver' => 'mysql'])
     ],
     'mariadb' => [
         'select * from `users` where `users`.`id` in (1, 2, 3) and `id` in (?, ?, ?)',
-        'select * from `users` where `users`.`id` in (...?) and `id` in (...?)',
+        hash('md5', 'foo,select * from `users` where `users`.`id` in (...?) and `id` in (...?)'),
         new MariaDbConnection('test', config: ['name' => 'foo', 'driver' => 'mariadb'])
     ],
     'pgsql' => [
         'select * from "users" where "users"."id" in (1, 2, 3) and "id" in (?, ?, ?)',
-        'select * from "users" where "users"."id" in (...?) and "id" in (...?)',
+        hash('md5', 'foo,select * from "users" where "users"."id" in (...?) and "id" in (...?)'),
         new PostgresConnection('test', config: ['name' => 'foo', 'driver' => 'pgsql'])
     ],
     'sqlite' => [
         'select * from "users" where "users"."id" in (1, 2, 3) and "id" in (?, ?, ?)',
-        'select * from "users" where "users"."id" in (...?) and "id" in (...?)',
+        hash('md5', 'foo,select * from "users" where "users"."id" in (...?) and "id" in (...?)'),
         new SQLiteConnection('test', config: ['name' => 'foo', 'driver' => 'sqlite'])
     ],
     'sqlsrv' => [
         'select * from [users] where [users].[id] in (1, 2, 3) and [id] in (?, ?, ?)',
-        'select * from [users] where [users].[id] in (...?) and [id] in (...?)',
+        hash('md5', 'foo,select * from [users] where [users].[id] in (...?) and [id] in (...?)'),
         new SqlServerConnection('test', config: ['name' => 'foo', 'driver' => 'sqlsrv'])
     ],
     'mongodb' => [
         'some mongo query in (1, 2, 3) and [id] in (?, ?, ?)',
-        'some mongo query in (1, 2, 3) and [id] in (?, ?, ?)',
+        hash('md5', 'foo,some mongo query in (1, 2, 3) and [id] in (?, ?, ?)'),
+        new MongoDbConnection(['name' => 'foo', 'driver' => 'mongodb', 'host' => 'localhost', 'database' => 'test'])
+    ],
+]);
+
+test('group hash collapses insert rows', function ($sql, $expected, $connection) {
+    $ingest = fakeIngest();
+    Route::get('/users', function () use ($sql, $connection) {
+        Event::dispatch(new QueryExecuted($sql, [], 1, $connection));
+    });
+
+    $response = get('/users');
+
+    $response->assertOk();
+    $ingest->assertWrittenTimes(1);
+    $ingest->assertLatestWrite('query:0.sql', $sql);
+    $ingest->assertLatestWrite('query:0._group', $expected);
+})->with([
+    'mysql one row' => [
+        'insert into `users` (`id`, `name`) values (?, ?)',
+        hash('md5', 'foo,insert into `users` (`id`, `name`) values ...'),
+        new MySqlConnection('test', config: ['name' => 'foo', 'driver' => 'mysql'])
+    ],
+    'mysql multiple rows' => [
+        'insert into `users` (`id`, `name`) values (?, ?), (?, ?)',
+        hash('md5', 'foo,insert into `users` (`id`, `name`) values ...'),
+        new MySqlConnection('test', config: ['name' => 'foo', 'driver' => 'mysql'])
+    ],
+    'mysql trailing stuff' => [
+        'insert into `users` (`id`, `name`) values (?, ?), (?, ?) on duplicate key update `name` = ?',
+        hash('md5', 'foo,insert into `users` (`id`, `name`) values ...on duplicate key update `name` = ?'),
+        new MySqlConnection('test', config: ['name' => 'foo', 'driver' => 'mysql'])
+    ],
+    'mariadb' => [
+        'insert into `users` (`id`, `name`) values (?, ?), (?, ?)',
+        hash('md5', 'foo,insert into `users` (`id`, `name`) values ...'),
+        new MariaDbConnection('test', config: ['name' => 'foo', 'driver' => 'mariadb'])
+    ],
+    'pgsql' => [
+        'insert into "users" ("id", "name") values (?, ?), (?, ?)',
+        hash('md5', 'foo,insert into "users" ("id", "name") values ...'),
+        new PostgresConnection('test', config: ['name' => 'foo', 'driver' => 'pgsql'])
+    ],
+    'sqlite' => [
+        'insert into "users" ("id", "name") values (?, ?), (?, ?)',
+        hash('md5', 'foo,insert into "users" ("id", "name") values ...'),
+        new SQLiteConnection('test', config: ['name' => 'foo', 'driver' => 'sqlite'])
+    ],
+    'sqlsrv' => [
+        'insert into [users] ([id], [name]) values (?, ?), (?, ?)',
+        hash('md5', 'foo,insert into [users] ([id], [name]) values ...'),
+        new SqlServerConnection('test', config: ['name' => 'foo', 'driver' => 'sqlsrv'])
+    ],
+    'mongodb' => [
+        'insert some mongo query values (?, ?), (?, ?)',
+        hash('md5', 'foo,insert some mongo query values (?, ?), (?, ?)'),
         new MongoDbConnection(['name' => 'foo', 'driver' => 'mongodb', 'host' => 'localhost', 'database' => 'test'])
     ],
 ]);
