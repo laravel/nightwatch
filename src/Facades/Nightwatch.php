0cedac349a9fc4e011acb731ce09abcc3d3c3631
Tim MacDonald <hello@timacdonald.me>
2024-12-18 15:11:44 +1100

    Add facade

---
+++ src/Facades/Nightwatch.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Laravel\Nightwatch\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Laravel\Nightwatch\Core
+ */
+class Nightwatch extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     */
+    public static function getFacadeAccessor(): string
+    {
+        return \Laravel\Nightwatch\Core::class;
+    }
+}
