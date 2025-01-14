feadb99c601fddddbbc510e43403e4c68231ace9
Ryuta Hamasaki <avosalmon@gmail.com>
2025-01-14 22:00:11 +0900

    Add more tests

--- tests/Feature/Sensors/ScheduledTaskSensorTest.php
+++ tests/Feature/Sensors/ScheduledTaskSensorTest.php
@@ -104,7 +104,7 @@ it('ingests skipped tasks', function () {
             'outgoing_requests' => 0,
             'files_read' => 0,
             'files_written' => 0,
-            'cache_events' => 1, // TODO: check what triggers cache events
+            'cache_events' => 1, // TODO: `ScheduledTaskStarting` event is not fired for skipped tasks and the counts are not reset.
             'hydrated_models' => 0,
             'peak_memory_usage' => 1234,
         ],
@@ -154,13 +154,106 @@ it('ingests failed tasks', function () {
     ]);
 });
 
+it('resets trace ID and timestamp before each task run', function () {
+    //
+})->todo();
+
+it('normalizes task name for named closure', function () {
+    $task = Schedule::call(fn () => travelTo(now()->addMicroseconds(1_000_000)))
+        ->name('named-closure')
+        ->everyMinute();
+
+    Artisan::call('schedule:run');
+
+    $this->ingest->assertWrittenTimes(1);
+    $this->ingest->assertLatestWrite('scheduled-task:*', [
+        [
+            'v' => 1,
+            't' => 'scheduled-task',
+            'timestamp' => 946688523.456789,
+            'deploy' => 'v1.2.3',
+            'server' => 'scheduler-01',
+            '_group' => hash('md5', "{$task->command},{$task->expression},{$task->timezone}"),
+            'trace_id' => '00000000-0000-0000-0000-000000000000',
+            'name' => 'named-closure',
+            'cron' => '* * * * *',
+            'timezone' => 'UTC',
+            'without_overlapping' => false,
+            'on_one_server' => false,
+            'run_in_background' => false,
+            'even_in_maintenance_mode' => false,
+            'status' => 'processed',
+            'duration' => 1_000_000,
+            'exceptions' => 0,
+            'logs' => 0,
+            'queries' => 0,
+            'lazy_loads' => 0,
+            'jobs_queued' => 0,
+            'mail' => 0,
+            'notifications' => 0,
+            'outgoing_requests' => 0,
+            'files_read' => 0,
+            'files_written' => 0,
+            'cache_events' => 0,
+            'hydrated_models' => 0,
+            'peak_memory_usage' => 1234,
+        ],
+    ]);
+});
+
+it('normalizes task name for invokable class', function () {
+    class ProcessFlights
+    {
+        public function __invoke()
+        {
+            travelTo(now()->addMicroseconds(1_000_000));
+        }
+    }
+
+    $task = Schedule::call(new ProcessFlights)->everyMinute();
+
+    Artisan::call('schedule:run');
+
+    $this->ingest->assertWrittenTimes(1);
+    $this->ingest->assertLatestWrite('scheduled-task:*', [
+        [
+            'v' => 1,
+            't' => 'scheduled-task',
+            'timestamp' => 946688523.456789,
+            'deploy' => 'v1.2.3',
+            'server' => 'scheduler-01',
+            '_group' => hash('md5', "{$task->command},{$task->expression},{$task->timezone}"),
+            'trace_id' => '00000000-0000-0000-0000-000000000000',
+            'name' => 'ProcessFlights',
+            'cron' => '* * * * *',
+            'timezone' => 'UTC',
+            'without_overlapping' => false,
+            'on_one_server' => false,
+            'run_in_background' => false,
+            'even_in_maintenance_mode' => false,
+            'status' => 'processed',
+            'duration' => 1_000_000,
+            'exceptions' => 0,
+            'logs' => 0,
+            'queries' => 0,
+            'lazy_loads' => 0,
+            'jobs_queued' => 0,
+            'mail' => 0,
+            'notifications' => 0,
+            'outgoing_requests' => 0,
+            'files_read' => 0,
+            'files_written' => 0,
+            'cache_events' => 0,
+            'hydrated_models' => 0,
+            'peak_memory_usage' => 1234,
+        ],
+    ]);
+});
+
 // it('normalizes task names', function (Event $task) {
 //     //
 // })
 //     ->with([
-//         'closure' => fn () => Schedule::call(fn () => null)->everyMinute(),
-//         'named closure' => fn () => Schedule::call(fn () => null)->name('named-closure')->everyMinute(),
-//         'invokable class' => fn () => Schedule::call()->everyMinute(),
 //         'artisan command signature' => fn () => Schedule::command('inspire')->everyMinute(),
 //         'artisan command class' => fn () => Schedule::command()->everyMinute(),
 //         'job' => fn () => Schedule::job(new class() {})->everyMinute(),
@@ -168,7 +261,3 @@ it('ingests failed tasks', function () {
 //         'shell command' => fn () => Schedule::exec('echo "Hello, world!"')->everyMinute(),
 //     ])
 //     ->todo();
-
-// it('resets trace ID and timestamp before each task run', function () {
-//     //
-// })->todo();
