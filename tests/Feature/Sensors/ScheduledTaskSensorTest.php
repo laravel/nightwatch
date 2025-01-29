f9ab8c2dacffc04241113150c5675d4bfc902d14
Ryuta Hamasaki <avosalmon@gmail.com>
2025-01-29 14:50:04 +0900

    Fix test

--- tests/Feature/Sensors/ScheduledTaskSensorTest.php
+++ tests/Feature/Sensors/ScheduledTaskSensorTest.php
@@ -2,12 +2,9 @@
 
 use Carbon\CarbonImmutable;
 use Illuminate\Bus\Queueable;
-use Illuminate\Console\Events\ScheduledTaskFinished;
-use Illuminate\Console\Events\ScheduledTaskStarting;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Testing\WithConsoleEvents;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Schedule;
 use Laravel\Nightwatch\Types\Str;
 
@@ -176,11 +173,6 @@ it('resets trace ID and timestamp on each task run', function () {
     $this->ingest->assertLatestWrite('scheduled-task:0.trace_id', '00000000-0000-0000-0000-000000000001');
     $this->ingest->assertLatestWrite('scheduled-task:0.timestamp', 946688523.456789);
     $this->ingest->flush();
-
-    // The event listers registered in the previous run are still active in testing.
-    // We need to forget them to avoid duplicate writes. It doesn't happen in the real application.
-    Event::forget(ScheduledTaskStarting::class);
-    Event::forget(ScheduledTaskFinished::class);
 
     Str::createUuidsUsing(fn () => '00000000-0000-0000-0000-000000000002');
     Artisan::call('schedule:run');
