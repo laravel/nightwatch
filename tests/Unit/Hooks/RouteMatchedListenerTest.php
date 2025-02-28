9e350845d65cd71b6c3df4cbb7666ed0e961d485
Tim MacDonald <hello@timacdonald.me>
2025-02-28 13:43:32 +1100

    Fix test

--- tests/Unit/Hooks/RouteMatchedListenerTest.php
+++ tests/Unit/Hooks/RouteMatchedListenerTest.php
@@ -1,10 +1,12 @@
 <?php
 
+use Illuminate\Foundation\Events\Terminating;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Events\RouteMatched;
 use Illuminate\Routing\Route;
 use Laravel\Nightwatch\Hooks\RouteMatchedListener;
 use Laravel\Nightwatch\Hooks\RouteMiddleware;
+use Laravel\Nightwatch\Hooks\TerminatingMiddleware;
 
 it('gracefully handles middleware registered as a string', function () {
     $request = Request::create('/users');
@@ -16,5 +18,9 @@ it('gracefully handles middleware registered as a string', function () {
 
     $handler($event);
 
-    expect($route->action['middleware'])->toBe(['api', RouteMiddleware::class]);
-})->skip();
+    if (class_exists(Terminating::class)) {
+        expect($route->action['middleware'])->toBe(['api', RouteMiddleware::class]);
+    } else {
+        expect($route->action['middleware'])->toBe([TerminatingMiddleware::class, 'api', RouteMiddleware::class]);
+    }
+});
