20c29df777d867f7c431c7f2b91bc1d0d1de950e
Jess Archer <jess@jessarcher.com>
2025-03-26 15:02:50 +1000

    Linting

--- tests/Unit/Hooks/RouteMatchedListenerTest.php
+++ tests/Unit/Hooks/RouteMatchedListenerTest.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Events\RouteMatched;
 use Illuminate\Routing\Route;
 use Laravel\Nightwatch\Compatibility;
+use Laravel\Nightwatch\Hooks\GlobalMiddleware;
 use Laravel\Nightwatch\Hooks\RouteMatchedListener;
 use Laravel\Nightwatch\Hooks\RouteMiddleware;
-use Laravel\Nightwatch\Hooks\GlobalMiddleware;
 
 it('gracefully handles middleware registered as a string', function () {
     $request = Request::create('/users');
