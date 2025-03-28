39a4129e2234b3c7f221ec91315ac59ce08f083e
Tim MacDonald <hello@timacdonald.me>
2025-03-28 15:51:24 +1100

    Add unrecoverable exception handling hooks

---
+++ tests/Feature/Facades/NightwatchTest.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\Log;
+use Laravel\Nightwatch\Core;
+use Laravel\Nightwatch\Facades\Nightwatch;
+
+it('resolves to bound singleton instance of the Core class', function () {
+    expect(Nightwatch::getFacadeRoot())->toBeInstanceOf(Core::class);
+
+    expect(Nightwatch::getFacadeRoot())->toBe(app(Core::class));
+
+    Facade::clearResolvedInstances();
+    expect(Nightwatch::getFacadeRoot())->toBe(app(Core::class));
+});
+
+it('silently discards unrecoverable exceptions by default', function () {
+    $calls = 0;
+    Log::listen(function () use (&$calls) {
+        $calls++;
+    });
+
+    Nightwatch::unrecoverableExceptionOccurred(new RuntimeException('Whoops!'));
+
+    expect($calls)->toBe(0);
+});
+
+it('can register a callback to handle unrecoverable exceptions', function () {
+    $handled = [];
+    Nightwatch::handleUnrecoverableExceptionsUsing(function (Throwable $e) use (&$handled) {
+        $handled[] = $e;
+    });
+
+    Nightwatch::unrecoverableExceptionOccurred($first = new RuntimeException('Whoops!'));
+    Nightwatch::unrecoverableExceptionOccurred($second = new RuntimeException('Whoops!'));
+
+    expect($handled)->toBe([
+        $first,
+        $second,
+    ]);
+});
+
+it('handles unrecoverable exceptions statelessly', function () {
+    app()->forgetInstance(Core::class);
+    $resolved = false;
+    Nightwatch::resolved(function () use (&$resolved) {
+        $resolved = true;
+    });
+
+    Nightwatch::handleUnrecoverableExceptionsUsing(function (Throwable $e) use (&$handled) {
+        $handled[] = $e;
+    });
+    Nightwatch::unrecoverableExceptionOccurred($first = new RuntimeException('Whoops!'));
+
+    expect($resolved)->toBeFalse();
+    expect(app()->resolved(Core::class))->toBeFalse();
+});
