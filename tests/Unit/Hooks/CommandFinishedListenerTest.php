8eb59bcc13f40bf6c9bef0741a99ba3725df5ae3
Tim MacDonald <hello@timacdonald.me>
2025-03-29 12:13:54 +1100

    wip

---
+++ tests/Unit/Hooks/CommandFinishedListenerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Console\Events\CommandFinished;
+use Laravel\Nightwatch\Compatibility;
+use Laravel\Nightwatch\ExecutionStage;
+use Laravel\Nightwatch\Hooks\CommandFinishedListener;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\NullOutput;
+
+it('gracefully handles exceptions', function () {
+    Compatibility::$terminatingEventExists = false;
+    $thrownInStageSensor = false;
+    nightwatch()->sensor->stageSensor = function () use (&$thrownInStageSensor) {
+        $thrownInStageSensor = true;
+
+        throw new RuntimeException('Whoops!');
+    };
+    nightwatch()->state->stage = ExecutionStage::Bootstrap;
+    nightwatch()->state->name = 'app:command';
+
+    $event = new CommandFinished(
+        'app:command', new StringInput('app:command'), new NullOutput, 1
+    );
+
+    $listener = new CommandFinishedListener(nightwatch());
+    $listener($event);
+
+    expect($thrownInStageSensor)->toBeTrue();
+    expect(nightwatch()->state->exceptions)->toBe(1);
+});
