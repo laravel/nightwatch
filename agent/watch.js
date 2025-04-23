9caa3c1abe155d9faf59234e3671393acc096d5d
Tim MacDonald <hello@timacdonald.me>
2025-04-23 11:46:42 +1000

    Add watch command

---
+++ agent/watch.js
@@ -0,0 +1,15 @@
+import chokidar from 'chokidar';
+import { execSync } from "node:child_process";
+
+const watching = chokidar.watch('./src', {
+    ignored: (path, stats) => stats?.isFile() && !path.endsWith('.php'),
+    ignoreInitial: true,
+})
+
+watching.on('all', (event, path) => {
+    console.log(`${event}: ${path}`)
+    console.log('> box compile')
+    console.log(execSync('box compile').toString('utf8'))
+})
+
+console.log(execSync('box compile').toString('utf8'))
