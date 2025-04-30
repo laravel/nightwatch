3b67ed871b9a2b32fd7c6cd3aa6db227868d9f10
Tim MacDonald <hello@timacdonald.me>
2025-04-30 11:41:42 +1000

    Add server tests

--- agent/tests/Feature/ServerTest.php
+++ agent/tests/Feature/ServerTest.php
@@ -8,6 +8,44 @@ use Tests\Response;
 use Tests\TcpServerFake;
 use Tests\Timer;
 
+it('server responds with OK', function () {
+    $loop = new LoopFake(runForSeconds: 2);
+    $server = new TcpServerFake;
+    $ingestDetailsBrowser = new BrowserFake([Response::jwt()]);
+    $ingestBrowser = new BrowserFake;
+
+    $loop->addTimer(1, $server->pendingConnection([['t' => 'request']]));
+
+    [$output, $e] = run(
+        via: 'source',
+        ingestDetailsBrowser: $ingestDetailsBrowser,
+        ingestBrowser: $ingestBrowser,
+        loop: $loop,
+        server: $server,
+    );
+
+    expect($e)->toBeNull($e?->getMessage() ?? '');
+    expect($server)->toHaveConnections([
+        Connection::closed('2:OK'),
+    ]);
+    expect($output)->toMatchLog(<<<'OUTPUT'
+        {date} {info} Authentication successful {duration}
+        OUTPUT);
+    expect($loop)->toHaveRun([
+        new Timer(interval: 1, runAt: 1, scheduledAt: 0, scheduledBy: self::class),
+    ]);
+    expect($loop)->toHavePending([
+        new Timer(interval: 10, runAt: 11, scheduledAt: 1, scheduledBy: 'Laravel\NightwatchAgent\Ingest::write'),
+        new Timer(interval: 3_600, runAt: 3_600, scheduledAt: 0, scheduledBy: 'Laravel\NightwatchAgent\IngestDetailsRepository::scheduleRefreshIn'),
+    ]);
+    expect($ingestDetailsBrowser)->toHaveSent([
+        Request::json('/api/agent-auth'),
+    ]);
+    expect($ingestDetailsBrowser)->toHavePending([]);
+    expect($ingestBrowser)->toHaveSentNothing();
+    expect($ingestBrowser)->toHavePending([]);
+});
+
 it('can ping the server', function () {
     $loop = new LoopFake(runForSeconds: 1);
     $server = new TcpServerFake;
@@ -26,7 +64,7 @@ it('can ping the server', function () {
 
     expect($e)->toBeNull($e?->getMessage() ?? '');
     expect($server)->toHaveConnections([
-        Connection::closed('4:PONG'),
+        Connection::closed('2:OK'),
     ]);
     expect($output)->toMatchLog(<<<'OUTPUT'
         {date} {info} Authentication successful {duration}
