7ef9656b9acd8f2bdb14cbe7f4268c6af69de59e
Tim MacDonald <hello@timacdonald.me>
2025-05-02 16:34:39 +1000

    Backport fake job

--- tests/FakeJob.php
+++ tests/FakeJob.php
@@ -3,9 +3,40 @@
 namespace Tests;
 
 use Illuminate\Contracts\Queue\Job as JobContract;
-use Illuminate\Queue\Jobs\FakeJob as JobsFakeJob;
+use Illuminate\Queue\Jobs\Job;
+use Illuminate\Support\Str;
 
-class FakeJob extends JobsFakeJob implements JobContract
+use function once;
+
+class FakeJob extends Job implements JobContract
 {
-    //
+    /**
+     * Get the job identifier.
+     *
+     * @return string
+     */
+    public function getJobId()
+    {
+        return once(fn () => (string) Str::uuid());
+    }
+
+    /**
+     * Get the raw body of the job.
+     *
+     * @return string
+     */
+    public function getRawBody()
+    {
+        return '';
+    }
+
+    /**
+     * Get the number of times the job has been attempted.
+     *
+     * @return int
+     */
+    public function attempts()
+    {
+        return 1;
+    }
 }
