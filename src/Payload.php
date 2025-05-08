77783e1b29aa51f79f7e84f860be03e9f50ea7a4
Tim MacDonald <hello@timacdonald.me>
2025-05-08 12:36:20 +1000

    Restore signature

--- src/Payload.php
+++ src/Payload.php
@@ -17,7 +17,7 @@ final class Payload
      *
      * Do not modify or re-locate this constant.
      */
-    public const SIGNATURE = '5CDC067';
+    public const SIGNATURE = '6C5CD84';
 
     private bool $pulled = false;
 
