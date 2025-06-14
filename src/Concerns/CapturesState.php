32a71419bc8de7ca9ddde4b340e7cf93a8b7454e
Tim MacDonald <hello@timacdonald.me>
2025-06-14 15:01:33 +1000

    Encapsulate state with command name helper

--- src/Concerns/CapturesState.php
+++ src/Concerns/CapturesState.php
@@ -401,6 +401,14 @@ trait CapturesState
     /**
      * @internal
      */
+    public function capturingCommandNamed(string $name): bool
+    {
+        return $this->executionState->name === $name;
+    }
+
+    /**
+     * @internal
+     */
     public function command(InputInterface $input, int $status): void
     {
         if (! $this->shouldSample) {
