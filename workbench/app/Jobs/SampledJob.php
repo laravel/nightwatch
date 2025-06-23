2adfe7ccfb034cf490e49c2f9afb7c7c3470a855
Tim MacDonald <hello@timacdonald.me>
2025-06-23 12:13:39 +1000

    wip

--- workbench/app/Jobs/SampledJob.php
+++ workbench/app/Jobs/SampledJob.php
@@ -2,13 +2,16 @@
 
 namespace App\Jobs;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Laravel\Nightwatch\Facades\Nightwatch;
 
 class SampledJob implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public function __construct(private float $rate)
     {
