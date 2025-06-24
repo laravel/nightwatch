d6bcc104ba450dd551a008b6f30994c641cf94aa
Jess Archer <jess@jessarcher.com>
2025-06-24 12:20:05 +1000

    Memoize the log handler level

--- src/Hooks/LogHandler.php
+++ src/Hooks/LogHandler.php
@@ -6,6 +6,7 @@ use Laravel\Nightwatch\Core;
 use Laravel\Nightwatch\State\CommandState;
 use Laravel\Nightwatch\State\RequestState;
 use Monolog\Handler\HandlerInterface;
+use Monolog\Level;
 use Monolog\Logger;
 use Monolog\LogRecord;
 use Throwable;
@@ -15,6 +16,8 @@ use Throwable;
  */
 final class LogHandler implements HandlerInterface
 {
+    protected Level $level;
+
     /**
      * @param  Core<RequestState|CommandState>  $nightwatch
      */
@@ -27,9 +30,9 @@ final class LogHandler implements HandlerInterface
 
     public function isHandling(LogRecord $record): bool
     {
-        $level = Logger::toMonologLevel($this->config['level'] ?? 'debug');
+        $this->level ??= Logger::toMonologLevel($this->config['level'] ?? 'debug');
 
-        return $this->nightwatch->shouldCaptureLogs() && $level->includes($record->level);
+        return $this->nightwatch->shouldCaptureLogs() && $this->level->includes($record->level);
     }
 
     public function handle(LogRecord $record): bool
