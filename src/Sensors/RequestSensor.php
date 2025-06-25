7a46ba033a15c0e95d050dfb2e274d93f56e7c38
Tim MacDonald <hello@timacdonald.me>
2025-06-25 16:48:43 +1000

    wip

--- src/Sensors/RequestSensor.php
+++ src/Sensors/RequestSensor.php
@@ -84,7 +84,7 @@ final class RequestSensor
                 'timestamp' => $this->requestState->timestamp,
                 'deploy' => $this->requestState->deploy,
                 'server' => $this->requestState->server,
-                '_group' => hash('xxh128', implode('|', $routeMethods).",{$routeDomain},{$routePath}"),
+                '_group' => hash('xxh128', implode('|', $record->routeMethods).",{$record->routeDomain},{$record->routePath}"),
                 'trace_id' => $this->requestState->trace,
                 'user' => $this->requestState->user->id(),
                 // --- //
