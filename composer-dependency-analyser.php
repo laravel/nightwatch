6ac71a64c057b2ce254f66bd470afcfa349bd162
Tim MacDonald <hello@timacdonald.me>
2025-06-26 17:33:39 +1000

    Ignore unused dep

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -6,4 +6,5 @@ use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 return (new Configuration)
     ->ignoreErrorsOnExtension('ext-zlib', [ErrorType::UNUSED_DEPENDENCY])
     ->ignoreErrorsOnPackageAndPath('spatie/laravel-ignition', __DIR__.'/src/Sensors/ExceptionSensor.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreErrorsOnPackageAndPath('spatie/laravel-ignition', __DIR__.'/src/Location.php', [ErrorType::DEV_DEPENDENCY_IN_PROD]);
+    ->ignoreErrorsOnPackageAndPath('spatie/laravel-ignition', __DIR__.'/src/Location.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
+    ->ignoreUnknownClasses(['Laravel\Octane\Events\RequestReceived']);
