ce117f730305e7672c8208546dfa6014e9f036b8
Tim MacDonald <hello@timacdonald.me>
2025-07-01 13:30:53 +1000

    Fix unit tests

--- tests/Unit/UserProviderTest.php
+++ tests/Unit/UserProviderTest.php
@@ -17,7 +17,7 @@ class UserProviderTest extends TestCase
         Auth::login(new GenericUser([
             'id' => str_repeat('x', 1000),
         ]));
-        $provider = new UserProvider($this->app['auth'], fn () => []);
+        $provider = new UserProvider($this->app['auth'], fn () => [], fn () => fn () => null);
 
         $this->assertSame(1000, strlen(Auth::id()));
         $this->assertSame($provider->id(), str_repeat('x', 255));
@@ -25,7 +25,7 @@ class UserProviderTest extends TestCase
 
     public function test_it_can_lazily_retrieve_the_user(): void
     {
-        $provider = new UserProvider($this->app['auth'], fn () => []);
+        $provider = new UserProvider($this->app['auth'], fn () => [], fn () => fn () => null);
 
         $id = $provider->id();
 
@@ -38,7 +38,7 @@ class UserProviderTest extends TestCase
 
     public function test_it_can_remember_an_authenticated_user_and_limits_the_length_of_their_identifier(): void
     {
-        $provider = new UserProvider($this->app['auth'], fn () => []);
+        $provider = new UserProvider($this->app['auth'], fn () => [], fn () => fn () => null);
         $provider->remember($user = new GenericUser([
             'id' => str_repeat('x', 1000),
         ]));
