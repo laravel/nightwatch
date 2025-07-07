1efd560609d4399809c6c0b9a4238093ee8be4f2
Ryuta Hamasaki <avosalmon@gmail.com>
2025-07-07 16:53:10 +0900

    Add aws/aws-sdk-php as dev dependency

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -9,6 +9,5 @@ return (new Configuration)
     ->ignoreErrorsOnPackageAndPath('spatie/laravel-ignition', __DIR__.'/src/Location.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
     ->ignoreErrorsOnPackageAndPath('livewire/livewire', __DIR__.'/src/NightwatchServiceProvider.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
     ->ignoreErrorsOnPackageAndPath('livewire/livewire', __DIR__.'/src/Hooks/LivewireListener.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreErrorsOnPackageAndPath('aws/aws-sdk-php', __DIR__.'/tests/Feature/Sensors/JobAttemptSensorTest.php', [ErrorType::SHADOW_DEPENDENCY])
     ->ignoreErrorsOnPath(__DIR__.'/tests/Feature/Sensors/QuerySensorTest.php', [ErrorType::UNKNOWN_CLASS])
     ->ignoreUnknownClasses(['Laravel\Octane\Events\RequestReceived']);
