f77e48ac27e178e795852d7e2bb3c1d2de67ec63
Jess Archer <jess@jessarcher.com>
2025-09-09 17:18:56 +1000

    Add failing test

--- tests/Unit/UserProviderTest.php
+++ tests/Unit/UserProviderTest.php
@@ -4,8 +4,11 @@ namespace Tests\Unit;
 
 use App\Models\User;
 use Illuminate\Auth\GenericUser;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+use Laravel\Nightwatch\Facades\Nightwatch;
 use RuntimeException;
 use Tests\TestCase;
 
@@ -155,5 +158,29 @@ class UserProviderTest extends TestCase
         $ingest->assertLatestWrite('exception:0.message', 'Whoops!');
         $ingest->assertLatestWrite('query:0.sql', 'select * from users');
         $ingest->assertLatestWrite('query:1.sql', 'select * from users');
+    }
+
+    public function test_the_user_id_can_be_customized(): void
+    {
+        $ingest = $this->fakeIngest();
+        Route::get('/users', fn () => User::all());
+        $user = User::make([
+            'id' => '456',
+            'name' => 'Tim MacDonald',
+            'email' => 'tim@laravel.com',
+        ]);
+        Nightwatch::user(fn (Authenticatable $user) => [
+            'id' => '123-'.$user->getAuthIdentifier(),
+            'name' => $user->name,
+            'username' => $user->email,
+        ]);
+
+        $response = $this->actingAs($user)->get('/users');
+
+        $response->assertOk();
+        $ingest->assertWrittenTimes(1);
+        $ingest->assertLatestWrite('user:0.id', '123-456');
+        $ingest->assertLatestWrite('query:0.user', '123-456');
+        $ingest->assertLatestWrite('request:0.user', '123-456');
     }
 }
