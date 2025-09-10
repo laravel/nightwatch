4bec84f91710eff206ae9f38257644e9799d3eb6
James Carpenter <james@james-carpenter.net>
2025-09-10 15:50:03 +1000

    chore: test changing composer install behaviour to try and create consistent build signatures

--- agent/build.sh
+++ agent/build.sh
@@ -4,7 +4,7 @@ set -e
 
 docker build --platform=linux/amd64 -t agent-builder -f docker/Dockerfile.build .
 
-docker run --rm -v $(pwd):/app --entrypoint composer agent-builder install
+docker run --rm -v $(pwd):/app --entrypoint composer agent-builder install --prefer-dist --no-dev --no-interaction --classmap-authoritative
 docker run --rm -v $(pwd):/app agent-builder
 SIGNATURE=$(docker run --rm -v $(pwd):/app agent-builder info:signature build/agent.phar)
 
