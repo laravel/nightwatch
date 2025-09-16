4d71bcafa2f72ac931ab1390d89db60b5093931f
Tim MacDonald <hello@timacdonald.me>
2025-09-16 10:51:39 +1000

    Improve test

--- tests/Feature/Console/AgentCommandTest.php
+++ tests/Feature/Console/AgentCommandTest.php
@@ -2,29 +2,32 @@
 
 namespace Tests\Feature\Console;
 
+use Illuminate\Process\Exceptions\ProcessTimedOutException;
 use Illuminate\Support\Facades\Process;
 use Tests\TestCase;
 
-use function collect;
-use function rescue;
 use function str_contains;
-use function with;
 
 class AgentCommandTest extends TestCase
 {
     public function test_it_can_run_the_agent_command(): void
     {
-        $output = collect();
-        $result = with(Process::timeout(5)->start('vendor/bin/testbench nightwatch:agent'), function ($process) use ($output) {
-            return rescue(fn () => $process->wait(function ($type, $o) use ($output) {
-                $output[] = $o;
+        $output = '';
+        $process = Process::timeout(5)->start('vendor/bin/testbench nightwatch:agent');
+        try {
+            $result = $process->wait(function ($type, $o) use (&$output, $process) {
+                $output .= $o;
 
                 if ($type === 'out' && str_contains($o, 'Authentication successful')) {
                     $process->signal(SIGINT);
                 }
-            }), report: false);
-        });
+            });
+        } catch (ProcessTimedOutException $e) {
+            echo $output;
 
-        $this->assertNotNull($result, $output->implode(''));
+            throw $e;
+        }
+
+        $this->assertStringContainsString('Authentication successful', $output);
     }
 }
