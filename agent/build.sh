11f9e7d166716a99176f1117bd14e1275f07f056
James Carpenter <james@james-carpenter.net>
2025-09-20 08:54:02 +1000

    chore: revert docker build to rule it out from causing issues

--- agent/build.sh
+++ agent/build.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-docker build --platform=linux/amd64 -t agent-builder -f docker/Dockerfile.build .
+box compile
 
-docker run --rm -v $(pwd):/app --entrypoint composer agent-builder install --prefer-dist --no-dev --no-interaction --classmap-authoritative
-docker run --rm -v $(pwd):/app agent-builder
-SIGNATURE=$(docker run --rm -v $(pwd):/app agent-builder info:signature build/agent.phar)
+SIGNATURE=$(box info:signature build/agent.phar)
 
 echo $SIGNATURE >build/signature.txt
