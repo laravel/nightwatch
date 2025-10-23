acc1505d337daeea1cead0322d7db8ba97a3fc4c
Jess Archer <jess@jessarcher.com>
2025-10-23 11:54:07 +1000

    Fix tests

--- tests/Feature/Console/DeployCommandTest.php
+++ tests/Feature/Console/DeployCommandTest.php
@@ -5,9 +5,11 @@ namespace Tests\Feature\Console;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
+use Laravel\Nightwatch\Console\DeployCommand;
 use Orchestra\Testbench\Attributes\WithEnv;
 use Tests\TestCase;
 
+use function env;
 use function now;
 
 class DeployCommandTest extends TestCase
@@ -19,7 +21,7 @@ class DeployCommandTest extends TestCase
         $this->freezeTime();
         Http::fake([
             '*/api/deployments' => function (Request $request) {
-                $this->assertEquals(['Bearer test-token'], $request->header('Authorization'));
+                $this->assertEquals(['Bearer '.env('NIGHTWATCH_TOKEN')], $request->header('Authorization'));
                 $this->assertEquals([
                     'timestamp' => now()->getTimestamp(),
                     'version' => 'v1.2.3',
@@ -40,7 +42,7 @@ class DeployCommandTest extends TestCase
         $this->freezeTime();
         Http::fake([
             '*/api/deployments' => function (Request $request) {
-                $this->assertEquals(['Bearer test-token'], $request->header('Authorization'));
+                $this->assertEquals(['Bearer '.env('NIGHTWATCH_TOKEN')], $request->header('Authorization'));
                 $this->assertEquals([
                     'timestamp' => now()->getTimestamp(),
                     'version' => '',
@@ -55,9 +57,10 @@ class DeployCommandTest extends TestCase
             ->assertExitCode(0);
     }
 
-    #[WithEnv('NIGHTWATCH_TOKEN', '')]
     public function test_it_fails_when_the_deploy_command_is_run_without_a_token(): void
     {
+        $this->app->singleton(DeployCommand::class, fn () => new DeployCommand(token: null));
+
         $this->artisan('nightwatch:deploy')
             ->expectsOutput('No NIGHTWATCH_TOKEN environment variable configured.')
             ->assertExitCode(1);
