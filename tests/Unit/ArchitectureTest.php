4fa200f636b0b0f5994dd5bf130cd284983b0478
Tim MacDonald <hello@timacdonald.me>
2025-11-17 11:03:33 +1100

    Formatting

--- tests/Unit/ArchitectureTest.php
+++ tests/Unit/ArchitectureTest.php
@@ -12,52 +12,49 @@ use function str_replace;
 
 class ArchitectureTest extends TestCase
 {
-    const SRC_PATH = __DIR__.'/../../src/';
+    public function test_classes_are_final(): void
+    {
+        foreach ($this->classes() as $class) {
+            if (! $class->isInterface() && ! $class->isTrait()) {
+                $this->assertTrue($class->isFinal(), "[{$class->getName()} is not final");
+            }
+        }
+    }
 
     public function test_classes_are_internal(): void
     {
         $except = [
-            \Laravel\Nightwatch\Records\QueuedJob::class,
-            \Laravel\Nightwatch\Records\Mail::class,
-            \Laravel\Nightwatch\Records\CacheEvent::class,
-            \Laravel\Nightwatch\Records\Request::class,
-            \Laravel\Nightwatch\Records\Command::class,
-            \Laravel\Nightwatch\Records\Query::class,
-            \Laravel\Nightwatch\Records\Notification::class,
-            \Laravel\Nightwatch\Records\Exception::class,
-            \Laravel\Nightwatch\Records\Exception::class,
-            \Laravel\Nightwatch\Records\OutgoingRequest::class,
-            \Laravel\Nightwatch\Http\Middleware\Sample::class,
             \Laravel\Nightwatch\Facades\Nightwatch::class,
+            \Laravel\Nightwatch\Http\Middleware\Sample::class,
+            \Laravel\Nightwatch\Records\CacheEvent::class,
+            \Laravel\Nightwatch\Records\Command::class,
+            \Laravel\Nightwatch\Records\Exception::class,
+            \Laravel\Nightwatch\Records\Exception::class,
+            \Laravel\Nightwatch\Records\Mail::class,
+            \Laravel\Nightwatch\Records\Notification::class,
+            \Laravel\Nightwatch\Records\OutgoingRequest::class,
+            \Laravel\Nightwatch\Records\Query::class,
+            \Laravel\Nightwatch\Records\QueuedJob::class,
+            \Laravel\Nightwatch\Records\Request::class,
         ];
 
         foreach ($this->classes() as $class) {
-            if (in_array($class->getName(), $except, true)) {
-                continue;
+            if (! in_array($class->getName(), $except, true)) {
+                $this->assertContains(' * @internal', explode("\n", $class->getDocComment()), "[{$class->getName()}] is not marked as internal. Add the @internal docblock tag to it or ignore it");
             }
-
-            $this->assertContains(' * @internal', explode("\n", $class->getDocComment()), "[{$class->getName()}] is not marked as internal. Add the @internal docblock tag to it or ignore it");
         }
     }
 
-    public function test_classes_are_final(): void
-    {
-        foreach ($this->classes() as $class) {
-            if ($class->isInterface() || $class->isTrait()) {
-                continue;
-            }
-
-            $this->assertTrue($class->isFinal(), "[{$class->getName()} is not final");
-        }
-    }
-
+    /**
+     * @return iterable<ReflectionClass>
+     */
     private function classes(): iterable
     {
-        $files = Finder::create()->files()->in(self::SRC_PATH);
+        $files = Finder::create()->files()->in($src = __DIR__.'/../../src/');
 
         foreach ($files as $file) {
             yield new ReflectionClass(
-                'Laravel\\Nightwatch\\'.str_replace([self::SRC_PATH, '.php', '/'], ['', '', '\\'], $file->getPathname())
+                'Laravel\\Nightwatch\\'.str_replace([$src, '.php', '/'], ['', '', '\\'], $file->getPathname())
             );
         }
     }
