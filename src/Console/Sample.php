0c4b6d84a0a934c13b7c0fa7f4bff7d146cc228c
jradtilbrook <jarryd@tilbrooktech.com>
2025-12-08 10:55:02 +0800

    Add alias methods

--- src/Console/Sample.php
+++ src/Console/Sample.php
@@ -10,8 +10,18 @@ use function app;
 
 final class Sample
 {
-    public static function rate(float $rate = 1.0): Closure
+    public static function rate(float $rate): Closure
     {
         return static fn (Event $event) => app(Core::class)->sampleScheduledTask($event, $rate);
+    }
+
+    public static function always(): Closure
+    {
+        return self::rate(1.0);
+    }
+
+    public static function never(): Closure
+    {
+        return self::rate(0.0);
     }
 }
