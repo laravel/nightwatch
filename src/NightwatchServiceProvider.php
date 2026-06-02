ec9ee7b94b679a4ea89df3d5d42d115502e62da1
Jarryd Tilbrook <jarryd@laravel.com>
2026-06-02 12:12:15 +0800

    Use cloud trace ID when present (#357)
    Co-authored-by: Tim MacDonald <hello@timacdonald.me>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Jess Archer <jess@jessarcher.com>

--- src/NightwatchServiceProvider.php
+++ src/NightwatchServiceProvider.php
@@ -242,7 +242,10 @@ final class NightwatchServiceProvider extends ServiceProvider
     {
         $clock = new Clock;
         $uuid = new Uuid(static fn () => BaseUuid::uuid4()->toString());
-        $executionState = $this->executionState($uuid->make());
+        $trace = $this->isRequest && Compatibility::$isLaravelCloud
+            ? ($_SERVER['HTTP_CLOUD_REQUEST_ID'] ?? $uuid->make())
+            : $uuid->make();
+        $executionState = $this->executionState($trace);
         $tokenHash = substr(hash('xxh128', $this->nightwatchConfig['token'] ?? ''), 0, 7);
 
         $this->app->instance(Core::class, $this->core = new Core(
